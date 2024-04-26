7c754f2b31c07c04e42d8aa0ef635563ccd91b6b
IxchelRmz <lunavaleria661@gmail.com>
2024-04-25 21:49:26 -0600

    Cambio2

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get('/', (req, res) => {
     res.send(`Hello World from Port ${port}`);
 });
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
     console.info(`Database is running on port ${port}`);
 });
+
+app.get('/myname', (req, res) => {
+    const name = 'Valeria'; // Reemplaza 'Tu nombre' con tu nombre real
+    res.send(`Hello World! My name is ${name}`);
+});
+
